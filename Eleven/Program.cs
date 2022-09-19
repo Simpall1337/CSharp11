25724b064c70933fe40121c34e98e0c519ed23a6
Alex Huzo <sanduguzo1337@gmail.com>
2022-09-19 13:22:56 +0300

    11

---
+++ Eleven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Lessons
+{
+    internal class Program
+    {
+        static void outputArray(ref int[] ArrayConsole, int i)
+        {
+            if (i == ArrayConsole.Length)
+                return;
+            Console.WriteLine(ArrayConsole[i]);
+            i++;
+            outputArray(ref ArrayConsole, i);
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5] { 1, 2, 33, 4, 5 };
+            outputArray(ref myArray, 0);
+        }
+    }
+}
